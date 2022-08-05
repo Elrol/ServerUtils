5f5af13a8cadbe1ff5a8ded2aa02d529e1ea6dee
Elrol <Elrol.Eyno@gmail.com>
2022-08-05 14:31:31 -0500

    Fixed error in DiscordMessageListener
    Signed-off-by: Elrol <Elrol.Eyno@gmail.com>

--- src/main/java/com/github/elrol/elrolsutilities/discord/events/DiscordMessageListener.java
+++ src/main/java/com/github/elrol/elrolsutilities/discord/events/DiscordMessageListener.java
@@ -6,6 +6,7 @@ import com.github.elrol.elrolsutilities.api.data.IPlayerData;
 import com.github.elrol.elrolsutilities.config.DiscordConfig;
 import com.github.elrol.elrolsutilities.discord.DiscordBot;
 import com.github.elrol.elrolsutilities.libs.text.TextUtils;
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.minecraft.util.text.ChatType;
@@ -18,28 +19,44 @@ public class DiscordMessageListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if(event.getAuthor().isBot()) return;
+        DiscordBot bot = Main.bot;
 
         long userID = event.getAuthor().getIdLong();
         IPlayerData data = IElrolAPI.getInstance().getPlayerDatabase().get(userID);
 
+        String displayName = data == null ? event.getAuthor().getName() : data.getDisplayName();
+        String sender = data == null ? event.getAuthor().getName() : bot.getName(data.getUUID());
         String content = event.getMessage().getContentDisplay();
 
         StringTextComponent text;
         if(data != null) {
             text = TextUtils.formatChat(data.getUUID(),content);
         } else {
-            String tag = DiscordConfig.discordTag.get();
+            String tag = Main.bot.getDiscordTag();
             String msg = (tag.isEmpty() ? "" : tag + "&r ") + event.getAuthor().getName() + ": " + content;
             text = new StringTextComponent(TextUtils.formatString(msg));
         }
 
         long id = event.getMessage().getGuildChannel().getIdLong();
-        DiscordBot bot = Main.bot;
 
-        if(bot.chatChannel.getIdLong() == id || bot.infoChannel.getIdLong() == id) {
+        if(bot.isChatChannel(id)) {
+            bot.chatChannels.forEach(c -> {
+                if(c.getIdLong() == id) return;
+                c.sendMessage(new MessageBuilder(TextUtils.stripFormatting(sender + content)).build()).queue();
+            });
             Main.mcServer.getPlayerList().broadcastMessage(text, ChatType.CHAT, bot.botUUID);
-        } else if(bot.staffChannel.getIdLong() == id) {
-            TextUtils.sendToStaff(event.getAuthor().getName(), bot.botUUID, content);
+        } else if(bot.isInfoChannel(id)) {
+            bot.infoChannels.forEach(c -> {
+                if(c.getIdLong() == id) return;
+                c.sendMessage(new MessageBuilder(TextUtils.stripFormatting(sender + content)).build()).queue();
+            });
+            Main.mcServer.getPlayerList().broadcastMessage(text, ChatType.CHAT, bot.botUUID);
+        } else if(bot.isStaffChannel(id)) {
+            bot.staffChannels.forEach(c -> {
+                if(c.getIdLong() == id) return;
+                c.sendMessage(new MessageBuilder(TextUtils.stripFormatting(sender + content)).build()).queue();
+            });
+            TextUtils.sendToStaff(displayName, bot.botUUID, content);
         }
         super.onMessageReceived(event);
     }
